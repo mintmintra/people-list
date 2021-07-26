32ea1a9afec366efbe220c5110e569e9dafa085a
mintmintra <mintracss019@gmail.com>
2021-07-26 16:11:39 +0700

    data prople

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Person=({img,name,job})=>{
+const Person=({img,name,job,children})=>{
   const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
   return(
     <article>
       <img src={url} alt="people"/>
       <h2>Name : {name}</h2>
       <h3>Job : {job}</h3>
+      {children}
     </article>
   )
 }
@@ -15,10 +16,18 @@ const Person=({img,name,job})=>{
 const PeopleList=()=>{
   return(
   <section>
-    <Person img="1" name="Tony" job="Programmer"/>
-    <Person img="17" name="John" job="Tester" />
-    <Person img="22" name="Thor" job="Designer" />
-    <Person img="5" name="Chris" job="Programmer" />
+    <Person img="1" name="Tony" job="Programmer">
+    <p>Skill : C# JAVA PHP</p>
+    </Person>
+    <Person img="17" name="John" job="Tester" >
+    <p>Skill : วิเคราะห์และทดสอบระบบ</p>
+    </Person>
+    <Person img="22" name="Thor" job="Designer" >
+    <p>Skill : Photoshop, 3D Model</p>
+    </Person>
+    <Person img="18" name="Nick" job="Designer">
+    <p>Skill : วิเคราะห์ออกแบบระบบ</p>
+    </Person>
   </section>
   )
 }
