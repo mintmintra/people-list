5cb336f370f20b8cc346bc870a358c3a72cae91b
mintmintra <mintracss019@gmail.com>
2021-07-26 22:24:34 +0700

    style css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css'
 const Person=({img,name,job,children})=>{
   const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
   return(
@@ -15,19 +15,68 @@ const Person=({img,name,job,children})=>{
 
 const PeopleList=()=>{
   return(
-  <section>
-    <Person img="1" name="Tony" job="Programmer">
-    <p>Skill : C# JAVA PHP</p>
-    </Person>
-    <Person img="17" name="John" job="Tester" >
-    <p>Skill : วิเคราะห์และทดสอบระบบ</p>
-    </Person>
-    <Person img="22" name="Thor" job="Designer" >
-    <p>Skill : Photoshop, 3D Model</p>
-    </Person>
-    <Person img="18" name="Nick" job="Designer">
-    <p>Skill : วิเคราะห์ออกแบบระบบ</p>
-    </Person>
+    <section className="person-list">
+      <h1>ข้อมูลบุคลากร</h1>
+      <div class="row">
+      <div class="column">
+        <div class="card">
+          <Person img="1" name="Tony" job="Programmer">
+            <p>Skill : C# JAVA PHP</p>
+          </Person>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <Person img="11" name="Tom" job="Data Science">
+            <p>Skill : PYTHON SQL R</p>
+          </Person>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <Person img="32" name="Nick" job="Desiner">
+            <p>Skill : HTML CSS PHOTOSHOP</p>
+          </Person>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <Person img="36" name="John" job="Tester">
+            <p>Skill : วิเคราะห์ทดสอบระบบ</p>
+          </Person>
+        </div>
+      </div>
+      </div>
+      <div class="row">
+      <div class="column">
+        <div class="card">
+          <Person img="34" name="Jojo" job="Desiner">
+            <p>Skill : PHOTOSHOP INDESIGN</p>
+          </Person>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <Person img="17" name="Conan" job="Programmer">
+            <p>Skill : HTML CSS JAVASCRIPT</p>
+          </Person>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <Person img="5" name="Loki" job="tester">
+            <p>Skill : วิเคราะห์ทดสอบระบบ</p>
+          </Person>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <Person img="8" name="Vin" job="Data Science">
+            <p>Skill : PYTHON SQL R</p>
+          </Person>
+        </div>
+      </div>
+      </div>
   </section>
   )
 }
